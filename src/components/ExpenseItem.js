c86a3f645aa8ca28aec301a694cd2c54313982f6
Muntaqa Mahmood <69706702+muntaqamahmood@users.noreply.github.com>
2023-09-19 22:35:19 -0400

    add: equivalent under the hood React code for JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,36 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 import Card from './Card';
+import React from 'react';
 
 export default function ExpenseItem(props) {
   // props is a key value pair of attributes from app.js
-  return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-    </Card>
+
+  // JSX code:
+  // return (
+  //   <Card className="expense-item">
+  //     <ExpenseDate date={props.date} />
+  //     <div className="expense-item__description">
+  //       <h2>{props.title}</h2>
+  //       <div className="expense-item__price">${props.amount}</div>
+  //     </div>
+  //   </Card>
+  // );
+
+  // equivalent under the hood React code that renders HTML on the UI:
+  return React.createElement(
+    Card,
+    { className: 'expense-item' },
+    React.createElement(ExpenseDate, { date: props.date }),
+    React.createElement(
+      'div',
+      { className: 'expense-item__description' },
+      React.createElement('h2', {}, props.title),
+      React.createElement(
+        'div',
+        { className: 'expense-item__price' },
+        props.amount
+      )
+    )
   );
 }
